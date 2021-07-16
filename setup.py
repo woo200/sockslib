0ac5c5e688921ff9b0f816a5d96f2dc1cb84394e
licyb200 <licyb200@protonmail.com>
2021-07-15 20:22:50 -0700

    Updated Version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="sockslib",
-    version="1.2.5",
+    version="1.2.6",
     description="Simple Socks5 proxy library",
     long_description=README,
     long_description_content_type="text/markdown",
--- sockslib/__init__.py
+++ sockslib/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.2.5"
+__version__ = "1.2.6"
 
 from sockslib.socks import *
 from sockslib.socksauth import *
