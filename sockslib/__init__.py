3690b15c74c8e93133bc95e5320301a66124ad34
licyb200 <licyb200@protonmail.com>
2021-07-15 20:08:52 -0700

    Restructuring

---
+++ sockslib/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "1.2.1"
+
+from sockslib.socks import *
+from sockslib.socksauth import *
---
+++ sockslib/socksauth.py
@@ -0,0 +1,15 @@
+from sockslib.socks import AuthenticationMethod
+
+class UserPassAuth(AuthenticationMethod):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def getId(self):
+        return 0x02
+
+    def authenticate(self, socket):
+        socket.sendall(b"\x01" + struct.pack("B", len(self.username)) + self.username.encode() + struct.pack("B", len(self.password)) + self.password.encode())
+        ver, status = socket.recv(2)
+
+        return status == 0x00
