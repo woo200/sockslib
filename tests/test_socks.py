ba27305f69c573c42087abcf50d58e6e7ccdbd2f
licyb200 <licyb200@protonmail.com>
2022-05-04 21:03:21 -0700

    Linting

--- tests/test_socks.py
+++ tests/test_socks.py
@@ -7,15 +7,16 @@ Reccomended to start a local TOR service when testing OR
 a proxy that runs on port 9050.
 """
 
+import sockslib
 import unittest
-import os, sys
+import sys
+import os
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 
 sys.path.append(parentdir)
 
-import sockslib
 
 class TestSocks(unittest.TestCase):
 
@@ -33,7 +34,7 @@ class TestSocks(unittest.TestCase):
         self.socket.connect(('1.1.1.1', 80))
         self.socket.sendall(b"GET / HTTP/1.1\r\nHost: 1.1.1.1\r\n\r\n")
 
-        self.socket.settimeout(10) # Timeout so recv don't go on forevcer
+        self.socket.settimeout(10)  # Timeout so recv don't go on forevcer
         response = self.socket.recv(1024)
 
         self.assertNotEqual(response, b'')
@@ -42,12 +43,12 @@ class TestSocks(unittest.TestCase):
         self.socket.connect(('myexternalip.com', 80))
         self.socket.sendall(b"GET /raw HTTP/1.1\r\nHost: 1.1.1.1\r\n\r\n")
 
-        self.socket.settimeout(10) # Timeout so recv don't go on forevcer
+        self.socket.settimeout(10)  # Timeout so recv don't go on forevcer
         response = self.socket.recv(1024)
 
         self.assertNotEqual(response, b'')
 
-    def test_socks5_udp(self): # UDP is extremely hard to test because hardly any proxies support it
+    def test_socks5_udp(self):  # UDP is extremely hard to test because hardly any proxies support it
         pass
 
     def test_socks5_noauth(self):
@@ -61,7 +62,7 @@ class TestSocks(unittest.TestCase):
 
     def test_socks5_general_error(self):
         with self.assertRaises(sockslib.SocksException):
-            self.socket.connect(('0.0.0.0', 80)) # Should not be able to connect
+            self.socket.connect(('0.0.0.0', 80))  # Should not be able to connect
 
     def test_socks4_direct(self):
         self.socket.set_proxy(('127.0.0.1', 9050),
@@ -70,7 +71,7 @@ class TestSocks(unittest.TestCase):
         self.socket.connect(('1.1.1.1', 80))
         self.socket.sendall(b"GET / HTTP/1.1\r\nHost: 1.1.1.1\r\n\r\n")
 
-        self.socket.settimeout(10) # Timeout so recv don't go on forevcer
+        self.socket.settimeout(10)  # Timeout so recv don't go on forevcer
         response = self.socket.recv(1024)
 
         self.assertNotEqual(response, b'')
@@ -80,7 +81,7 @@ class TestSocks(unittest.TestCase):
                               sockslib.Socks.SOCKS4)
 
         with self.assertRaises(sockslib.SocksException):
-            self.socket.connect(('0.0.0.0', 80)) # Should not be able to connect
+            self.socket.connect(('0.0.0.0', 80))  # Should not be able to connect
 
     def test_socks4_ipv6_deny(self):
         self.socket.set_proxy(('127.0.0.1', 9050),
@@ -92,6 +93,7 @@ class TestSocks(unittest.TestCase):
     def test_socks_udp_fail(self):
         with self.assertRaises(sockslib.SocksException):
             self.socket.initudp()
+
 
 if __name__ == '__main__':
     unittest.main()
